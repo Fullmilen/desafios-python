07969d3405933165ad133fff33b1f09cc1f36622
Milena de Oliveira <milena.oliv2016@gmail.com>
2022-09-12 16:05:57 -0300

    Update exercício7.py

--- exercício7.py
+++ exercício7.py
@@ -1,3 +1,7 @@
+#Desenvolva um gerador de tabuada, capaz de gerar a tabuada de qualquer
+#número inteiro entre 1 a 10. O usuário deve informar de qual número ele
+#deseja ver a tabuada.
+
 val = int(input('Digite um valor: '))
 
 for count in range (10):
