1448d9004a6e83c3bff900bc5bcdb2ab48131e78
Milena de Oliveira <milena.oliv2016@gmail.com>
2022-09-12 16:04:07 -0300

    Update exercício5.py

--- exercício5.py
+++ exercício5.py
@@ -1,3 +1,9 @@
+#Faça um programa para a leitura de duas notas parciais de um aluno.
+#O programa deve calcular a média alcançada por aluno e apresentar: o A
+#mensagem "Aprovado", se a média alcançada for maior ou igual a sete;
+#o A mensagem "Reprovado", se a média for menor do que sete; o A
+#mensagem "Aprovado com Distinção", se a média for igual a dez.
+
 n1 = float(input('Digite a primeira nota para ser calculada a sua média: '))
 n2 = float(input('Digite a segunda nota: '))
 media = (n1 + n2) / 2
