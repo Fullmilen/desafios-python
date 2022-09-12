ee94fb4ef744a1878c52146c77fa75a2784e468f
Milena de Oliveira <milena.oliv2016@gmail.com>
2022-09-12 15:49:53 -0300

    Update exercício1.py

--- exercício1.py
+++ exercício1.py
@@ -1,3 +1,4 @@
+##1. Faça um Programa que peça dois números e imprima a soma.
 n1 = float(input('Digite um número qualquer: '))
 n2 = float(input('Digite outro número: '))
 print('A soma de {} e {} é: {}. '.format(n1, n2, n1+n2))
