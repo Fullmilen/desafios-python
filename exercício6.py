b442fcbf9ea9b8309e0b352ed7b0bf3da5748f62
Milena de Oliveira <milena.oliv2016@gmail.com>
2022-09-12 16:05:00 -0300

    Update exercício6.py

--- exercício6.py
+++ exercício6.py
@@ -1,3 +1,15 @@
+#As Organizações Tabajara resolveram dar um aumento de salário aos seus
+#colaboradores e lhe contraram para desenvolver o programa que calculará os
+#reajustes.
+#Faça um programa que recebe o salário de um colaborador e o reajuste
+#segundo o seguinte critério, baseado no salário atual:
+#o salários até R$ 280,00 (incluindo) : aumento de 20% o salários entre R$
+#280,00 e R$ 700,00 : aumento de 15% o salários entre R$ 700,00 e R$
+#1500,00 : aumento de 10% o salários de R$ 1500,00 em diante : aumento
+#de 5% Após o aumento ser realizado, informe na tela: o o salário antes do
+#reajuste; o o percentual de aumento aplicado; o o valor do aumento; o o
+#novo salário, após o aumento.
+
 salario = float(input('Informe o seu salário para ser calculado o reajuste: '))
 au1 = (salario * 20) / 100
 au2 = (salario * 15) / 100
